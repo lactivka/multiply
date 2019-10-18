8f2967b20f6186ab636f7dad493dea3eeec6fc83
Stepanenko Oksana <lactivo4ka@gmail.com>
2019-10-18 23:04:43 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function multiply(first, second) {
   // your solution
+  return (String(BigInt(first) * BigInt(second))); // use BigInt to be able multiply very long numbers and use String to show result as a string
 }
